1e47901c0143c953babed0253b6bd8c4bb76f841
Thomas Garver <tgarv@mit.edu>
2015-01-30 16:03:38 +0000

    Listening on correct heroku port

--- flask_app/app.py
+++ flask_app/app.py
@@ -79,5 +79,5 @@ def create_app(settings_key='dev'):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
-    # app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
