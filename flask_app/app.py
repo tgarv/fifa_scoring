2890e5aa0df9eccac8c7e0252c4962b4d4799a20
Thomas Garver <tgarv@mit.edu>
2015-01-30 15:18:20 +0000

    Adding files in prep for heroku deploy

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import sqlite3
 import queries
@@ -20,11 +20,59 @@ def create_app(settings_key='dev'):
         player_results = cursor.fetchall()
         cursor.execute(queries.team_results_query)
         team_results = cursor.fetchall();
-        return render_template('index.html', player_results=player_results, team_results=team_results)
+        cursor.execute(queries.game_history_query)
+        game_history = cursor.fetchall()
+        game_history = deduplicate_game_players(game_history)
+        print game_history
+        return render_template('index.html', player_results=player_results, team_results=team_results, game_history=game_history)
         return json.dumps({'player_results': str(player_results), 'team_results': str(team_results)})
+
+    @app.route('/add_game', methods=['POST'])
+    def add_game():
+        print request.form
+        conn = sqlite3.connect('/home/ubuntu/workspace/scores.db')
+        cursor = conn.cursor()
+        # print (request.form['date'], request.form['away_team'], request.form['home_team'], request.form['away_score'], request.form['home_score'], request.form['half_length'])
+        sql = "INSERT INTO game (date, away_team, home_team, away_score, home_score, half_length) VALUES (?, ?, ?, ?, ?, ?);"
+        cursor.execute(sql, (request.form['date'], request.form['away_team'], request.form['home_team'], request.form['away_score'], request.form['home_score'], request.form['half_length']))
+        game_id = cursor.lastrowid
+        away_players = request.form['away_players'].split(',')
+        for player in away_players:
+            sql = "INSERT INTO away_players (game_id, player_name) VALUES (?, ?)";
+            cursor.execute(sql, (game_id, player))
+        home_players = request.form['home_players'].split(',')
+        for player in home_players:
+            sql = "INSERT INTO home_players (game_id, player_name) VALUES (?, ?)";
+            cursor.execute(sql, (game_id, player))
+        conn.commit()
+        return "here"
 
     def get_cursor():
         return sqlite3.connect('/home/ubuntu/workspace/scores.db').cursor()
+
+    def deduplicate_game_players(game_history):
+        return_value = []
+        for game in game_history:
+            game_dict = {
+                'date': game[0],
+                'home_players': game[1],
+                'home_team': game[2],
+                'home_score': game[3],
+                'away_players': game[4],
+                'away_team': game[5],
+                'away_score': game[6]
+                }
+
+            home_players = game[1]
+            away_players = game[4]
+            # Turn these into sets to deduplicate the names.
+            # This is obviously a hack, I should actually fix the query.
+            home_players = set(home_players.split('-'))
+            away_players = set(away_players.split('-'))
+            game_dict['home_players'] = '/'.join(home_players)
+            game_dict['away_players'] = '/'.join(away_players)
+            return_value.append(game_dict)
+        return return_value
 
     return app
 
--- flask_app/queries.py
+++ flask_app/queries.py
@@ -1,20 +1,20 @@
 total_goals_query = """SELECT name, SUM(score) FROM (SELECT player_name as name, home_score as score FROM home_players INNER JOIN game ON game_id = id UNION ALL SELECT player_name as name, away_score as score FROM away_players INNER JOIN game ON game_id = id) GROUP BY name"""
 team_goals_query = """
 SELECT team_players, SUM(score), AVG(score), COUNT(score)
-FROM 
+FROM
     (SELECT group_concat(player_name, '-') as team_players,
             away_score as score
-    FROM 
+    FROM
         (SELECT player_name, game_id
          FROM away_players ORDER BY player_name
         )
-    INNER JOIN game 
+    INNER JOIN game
         ON game_id = id
     GROUP BY game_id
-    UNION ALL 
+    UNION ALL
     SELECT group_concat(player_name, '-') as team_players,
            home_score as score
-    FROM 
+    FROM
         (SELECT player_name, game_id
          FROM home_players ORDER BY player_name
         )
@@ -23,30 +23,74 @@ FROM
     GROUP BY game_id)
 GROUP BY team_players;"""
 
-player_wins_query = """SELECT name, COUNT(*) FROM (SELECT player_name as name, home_score as score FROM home_players INNER JOIN game ON game_id = id WHERE home_score > away_score UNION ALL SELECT player_name as name, away_score as score FROM away_players INNER JOIN game ON game_id = id WHERE away_score > home_score) GROUP BY name;"""
-player_game_results_query = """SELECT name, SUM(win) as wins, SUM(loss) as losses, SUM(tie) as ties FROM (SELECT player_name as name, home_score as score, home_score > away_score as win, home_score < away_score as loss, home_score = away_score as tie FROM home_players INNER JOIN game ON game_id = id UNION ALL SELECT player_name as name, away_score as score, away_score > home_score as win, away_score < home_score as loss, away_score = home_score as tie FROM away_players INNER JOIN game ON game_id = id) GROUP BY name;"""
+player_wins_query = """SELECT name, COUNT(*)FROM (SELECT player_name as name, home_score as score FROM home_players INNER JOIN game ON game_id = id WHERE home_score > away_score UNION ALL SELECT player_name as name, away_score as score FROM away_players INNER JOIN game ON game_id = id WHERE away_score > home_score) GROUP BY name;"""
+player_game_results_query = """
+SELECT  name,
+        COUNT(*) as games_played,
+        SUM(win) as wins,
+        SUM(loss) as losses,
+        SUM(tie) as ties,
+        SUM(score),
+        AVG(score),
+        SUM(opponent_score),
+        AVG(opponent_score),
+        SUM(score) - SUM(opponent_score) as goal_differential,
+        (SUM(score) - SUM(opponent_score)) * 1.0 / COUNT(*) as average_goal_differential
+FROM
+    (SELECT player_name as name,
+            home_score as score,
+            away_score as opponent_score,
+            home_score > away_score as win,
+            home_score < away_score as loss,
+            home_score = away_score as tie
+     FROM home_players
+     INNER JOIN game
+        ON game_id = id
+    UNION ALL
+     SELECT player_name as name,
+            away_score as score,
+            home_score as opponent_score,
+            away_score > home_score as win,
+            away_score < home_score as loss,
+            away_score = home_score as tie
+     FROM away_players
+     INNER JOIN game
+        ON game_id = id
+    ) GROUP BY name;"""
 team_results_query = """
-SELECT team_players, SUM(win) as wins, SUM(loss) as losses, SUM(tie) as ties
-FROM 
+SELECT  team_players,
+        COUNT(*) as games_played,
+        SUM(win) as wins,
+        SUM(loss) as losses,
+        SUM(tie) as ties,
+        SUM(score) as score,
+        AVG(score) as average_score,
+        SUM(opponent_score) as opponent_score,
+        AVG(opponent_score) as average_opponent_score,
+        SUM(score) - SUM(opponent_score) as goal_differential,
+        (SUM(score) - SUM(opponent_score)) * 1.0 / COUNT(*) as average_goal_differential
+FROM
     (SELECT group_concat(player_name, '-') as team_players,
             away_score as score,
+            home_score as opponent_score,
             away_score > home_score as win,
             away_score = home_score as tie,
             away_score < home_score as loss
-    FROM 
+    FROM
         (SELECT player_name, game_id
          FROM away_players ORDER BY player_name
         )
-    INNER JOIN game 
+    INNER JOIN game
         ON game_id = id
     GROUP BY game_id
-    UNION ALL 
+    UNION ALL
     SELECT group_concat(player_name, '-') as team_players,
            home_score as score,
+           away_score as opponent_score,
            home_score > away_score as win,
            home_score = away_score as tie,
            home_score < away_score as loss
-    FROM 
+    FROM
         (SELECT player_name, game_id
          FROM home_players ORDER BY player_name
         )
@@ -54,3 +98,9 @@ FROM
         ON game_id = id
     GROUP BY game_id)
 GROUP BY team_players;"""
+
+
+"""SELECT date, group_concat(hp.player_name, '-'), home_team, home_score, group_concat(ap.player_name, '-'), away_team, away_score"""
+
+# TODO this query isn't correct -- the multiple inner joins cause duplication of player names
+game_history_query = """SELECT date, group_concat(hp.player_name, '-'), home_team, home_score, group_concat(ap.player_name, '-'), away_team, away_score FROM game INNER JOIN away_players ap ON ap.game_id = id INNER JOIN home_players hp ON hp.game_id = id GROUP BY id ORDER BY date DESC;"""
