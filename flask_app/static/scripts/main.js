7b651dd8e65cd6a8624d5eda6c6cfabd4f1dbf3e
Thomas Garver <tgarv@mit.edu>
2015-04-24 18:04:21 +0000

    Adding datepicker

--- flask_app/static/scripts/main.js
+++ flask_app/static/scripts/main.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('#player_results').dataTable({"paging": false, "searching": false, "order": [2, "desc"], "autoWidth": true});
     $('#team_results').dataTable({"paging": false, "searching": false, "order": [2, "desc"], "autoWidth": true});
     $('#club_results').dataTable({"paging": false, "searching": false, "order": [2, "desc"], "autoWidth": true});
-    $('#game_history').dataTable({"paging": false, "searching": false, "order": [0, "desc"], "autoWidth": true});
+    $('#game_history').dataTable({"paging": true, "searching": false, "order": [0, "desc"], "autoWidth": true, "pageLength": 25});
+    $( ".datepicker" ).datepicker({dateFormat: "yy-mm-dd"});
 } );
